c815d810bd05b569438edc27cb6670df3a843736
Jerrick Warren <Jerrick.Warren@gmail.com>
2019-09-09 11:19:17 -0700

    Updated APIService

--- JobCoin Wallet/JobCoin Wallet/APIService/APIService.swift
+++ JobCoin Wallet/JobCoin Wallet/APIService/APIService.swift
@@ -18,13 +18,13 @@ enum APIError: Error {
 }
 
 class APIService {
-   
+    
     // TODO: build URLS from base URL
     
     var tc = TransactionViewController()
     let baseURL = "http://jobcoin.gemini.com/helpless/api/"
     static let shared = APIService()
-   
+    
     
     enum APIError: Error {
         case responseProblem
@@ -63,10 +63,10 @@ class APIService {
         }
         dataTask.resume()
     }
-
+    
     //MARK: - GET User Balance
     func getUserBalance(user: String, completion: @escaping (Result<Balance, APIError>) -> Void)  {
-    
+        
         let jsonURLString = "https://jobcoin.gemini.com/helpless/api/addresses/\(user)"
         
         guard let url = URL(string: jsonURLString) else { return }
@@ -101,41 +101,39 @@ class APIService {
     
     // MARK: - POST Transactions
     func postTransaction(transaction: Transaction, completion: @escaping (Result<Transaction, APIError>) -> Void) {
-    
-    do {
-        guard  let url = URL(string: "http://jobcoin.gemini.com/helpless/api/transactions") else { fatalError() }
-        var urlRequest = URLRequest(url: url)
-        urlRequest.httpMethod = "POST"
-        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        urlRequest.httpBody = try JSONEncoder().encode(transaction)
         
-        let dataTask = URLSession.shared.dataTask(with: urlRequest) { _, response, error in
+        do {
+            guard  let url = URL(string: "http://jobcoin.gemini.com/helpless/api/transactions") else { fatalError() }
+            var urlRequest = URLRequest(url: url)
+            urlRequest.httpMethod = "POST"
+            urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            urlRequest.httpBody = try JSONEncoder().encode(transaction)
             
-            if let error = error {
-                NSLog("Error performing PUT request: \(error.localizedDescription)")
-                completion(.failure(.otherProblem))
-                return }
-            
-            guard let httpResponse = response as? HTTPURLResponse else {
-                completion(.failure(.responseProblem))
-                return }
-            
-            if httpResponse.statusCode == 200 {
-                print("OK Transaction was sent! ")
-            } else if httpResponse.statusCode == 422 {
-                // Create and UIAlert
-                print("Insufficent Funds")
-            } else {
-                print("Unknown Response")
+            let dataTask = URLSession.shared.dataTask(with: urlRequest) { _, response, error in
+                
+                if let error = error {
+                    NSLog("Error performing PUT request: \(error.localizedDescription)")
+                    completion(.failure(.otherProblem))
+                    return }
+                
+                guard let httpResponse = response as? HTTPURLResponse else {
+                    completion(.failure(.responseProblem))
+                    return }
+                
+                if httpResponse.statusCode == 200 {
+                    print("OK Transaction was sent! ")
+                } else if httpResponse.statusCode == 422 {
+                    print("Insufficent Funds")
+                } else {
+                    print("Unknown Response")
+                }
+                
             }
-            
+            dataTask.resume()
+        } catch {
+            completion(.failure(.encodingProblem))
         }
-        dataTask.resume()
-    } catch {
-        completion(.failure(.encodingProblem))
     }
 }
 
-}
-    
 
